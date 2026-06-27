1612e74c41f1e1c9ea1d3b3c3057e0747f0dbc75
MigalLewis <migallewis@gmail.com>
2026-06-27 10:19:58 +0200

    ci: fix karma adapter and gate firebase deploy

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,7 +1,16 @@
 // CI-compatible Karma configuration.
-// Uses ChromeHeadless with deterministic, sandbox-free flags that work on GitHub Actions Linux runners.
+// Angular's test builder uses this full config so Jasmine's adapter can call __karma__.start().
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -18,10 +27,15 @@ module.exports = function (config) {
     singleRun: true,
     restartOnFileChange: false,
     autoWatch: false,
-    reporters: ['progress'],
+    reporters: ['progress', 'kjhtml'],
     client: {
       clearContext: false,
       jasmine: { random: false }
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/invoice'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     }
   });
 };
